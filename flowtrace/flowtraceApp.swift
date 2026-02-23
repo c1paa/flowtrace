d7e86688633acf13225a052613873b2fe117f641
vlad <vladglazunov621@gmail.com>
2026-02-23 23:10:20 +0300

    Initial Commit

---
+++ flowtrace/flowtraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  flowtraceApp.swift
+//  flowtrace
+//
+//  Created by Vlad Glazunov on 23.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct flowtraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ flowtraceTests/flowtraceTests.swift
@@ -0,0 +1,17 @@
+//
+//  flowtraceTests.swift
+//  flowtraceTests
+//
+//  Created by Vlad Glazunov on 23.02.2026.
+//
+
+import Testing
+@testable import flowtrace
+
+struct flowtraceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
